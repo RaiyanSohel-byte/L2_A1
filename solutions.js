a3b984fefaf7335508c10db728b54268b3f93ee7
Raiyan Sohel <raiyansohel22@gmail.com>
2026-07-08 04:12:58 +0600

    solution-6 and 7 added

--- solutions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let hello = "String";
-console.log(hello);
-//# sourceMappingURL=solutions.js.map
--- solutions.ts
+++ solutions.ts
@@ -17,7 +17,51 @@ function checkType(value: StringOrNumber): string {
   }
   return "Number";
 }
+
 //Solution-4
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
+}
+
+//Solution-5
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book) {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+//Solution-6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+//Solution-7
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((num) => arr2.includes(num));
 }
